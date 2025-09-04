8698b7531936971192f120283717a8a3f7f30886
yogesh chaudhary <yogeshchaudhary815>
2025-09-04 20:05:26 +0530

    feat: add routing and main components for Spotify clone

---
+++ src/components/DisplayHome.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+function DisplayHome() {
+  return (
+    <>
+    <Navbar />
+    </>
+  )
+}
+
+export default DisplayHome
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -14,6 +14,29 @@ const Sidebar = () => {
       <p className='font-bold'>Search</p>
     </div>
      </div>
+
+     <div className='bg-[#121212] h-[85%] rounded'>
+      <div className='p-4 flex items-center justify-between'>
+       <div className='flex items-center gap-3'>
+         <img className='w-8' src={assets.stack_icon} alt="" />
+         <p className='font-semibold'>Your Library</p>
+       </div>
+       <div className='flex items-center gap-3'>
+         <img className='w-5' src={assets.arrow_icon} alt="" />
+         <img className='w-5' src={assets.plus_icon} alt="" />
+       </div>
+      </div>
+      <div className='p-4 bg-[#242424] m-2 rounded font-semibold items-start justify-start gap-1 pl-4'>
+       <h1>Create your first Playlist</h1>
+       <p className='font-light'>it's easy we will help you</p>
+       <button className='px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4'>Create Playlist</button>
+      </div>
+      <div className='p-4 bg-[#242424] m-2 rounded font-semibold items-start justify-start gap-1 pl-4'>
+       <h1>Let's findsome podcasts to follow</h1>
+       <p className='font-light'>we'll keep you update on new episodes</p>
+       <button className='px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4'>Browse Podcast</button>
+      </div>
+    </div>
     </div>
   )
 }
